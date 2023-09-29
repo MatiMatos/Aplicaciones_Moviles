2931f434d4c0f7cfd24533abfa49f6138a561b39
MatiMatos <matias.castillop@usm.cl>
2023-09-29 12:45:00 -0300

    Termino de API de monedas

--- flutter_application_12/lib/pages/tabs_calculadora.dart
+++ flutter_application_12/lib/pages/tabs_calculadora.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:loading_progress/loading_progress.dart';
+import 'package:flutter_application_12/providers/moneda_provider.dart';
+//import 'package:loading_progress/loading_progress.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class TabsCalcuradoraPage extends StatefulWidget {
   const TabsCalcuradoraPage({super.key});
@@ -9,23 +11,50 @@ class TabsCalcuradoraPage extends StatefulWidget {
 }
 
 class _TabsCalcuradoraPageState extends State<TabsCalcuradoraPage> {
+  MonedasProvider monedas = MonedasProvider();
+
+  String monedasSeleccionada = "";
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        ElevatedButton(
-            onPressed: onTapStartbtn, child: const Text('Calculadora'))
-      ],
-    ));
-  }
-
-  onTapStartbtn() async {
-    LoadingProgress.start(
-      context,
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Column(
+        children: [
+          Expanded(
+              child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10.0),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Monto CPL',
+                    hintText: 'Monto en peso Chileno',
+                    suffixIcon: Icon(MdiIcons.cash),
+                  ),
+                ),
+              )
+            ],
+          )),
+        ],
+      ),
     );
-    await Future.delayed(const Duration(seconds: 2));
-    LoadingProgress.stop(context);
+
+    ///Center(
+    ///child: Column(
+    ///mainAxisAlignment: MainAxisAlignment.center,
+    ///children: [
+    ///ElevatedButton(
+    /// onPressed: onTapStartbtn, child: const Text('Calculadora'))
+    ///      ],
+    ///    ));
+    ///  }
+
+    ///  onTapStartbtn() async {
+    ///    LoadingProgress.start(
+    ///      context,
+    ///    );
+    ///    await Future.delayed(const Duration(seconds: 2));
+    ///    LoadingProgress.stop(context);
   }
 }
--- flutter_application_12/lib/pages/tabs_monedas.dart
+++ flutter_application_12/lib/pages/tabs_monedas.dart
@@ -1,17 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_12/providers/moneda_provider.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class TabsMonedasPage extends StatefulWidget {
   const TabsMonedasPage({super.key});
-
   @override
   State<TabsMonedasPage> createState() => _TabsMonedasPageState();
 }
 
 class _TabsMonedasPageState extends State<TabsMonedasPage> {
+  MonedasProvider monedas = MonedasProvider();
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Monedas'),
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Column(
+        children: [
+          Container(
+            child: Text('Lista Monedas', style: TextStyle(fontSize: 23)),
+          ),
+          Expanded(
+              child: FutureBuilder(
+                  future: monedas.getMonedas(),
+                  builder: (context, AsyncSnapshot snapshot) {
+                    if (!snapshot.hasData) //Los datos NO LLEGARON
+                    {
+                      return Center(
+                        child: Container(
+                          width: 200,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('Cargando.'),
+                              LinearProgressIndicator()
+                              //CircularProgressIndicator()
+                            ],
+                          ),
+                        ),
+                      );
+                    } else {
+                      //Leega respuesta
+                      return ListView.separated(
+                        separatorBuilder: (_, __) => Divider(),
+                        itemCount: snapshot.data.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            leading: Icon(MdiIcons.cash),
+                            title: Text(snapshot.data[index]['Codigo']),
+                            subtitle: Text(snapshot.data[index]['Nombre']),
+                            trailing: Text(snapshot.data[index]['Valor']),
+                          );
+                        },
+                      );
+                    }
+                  }))
+        ],
+      ),
     );
   }
 }
